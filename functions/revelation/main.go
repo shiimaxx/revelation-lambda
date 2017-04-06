db525f5399ae4dd9303593dba8d07c33af41b74b
YOSHIMA Takatada <yoshima@shiimaxx.com>
2017-04-06 23:02:40 +0900

    Work in progress

---
+++ functions/revelation/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+
+	"github.com/apex/go-apex"
+
+	"github.com/shiimaxx/revelation"
+)
+
+type message struct {
+	Hello string `json:"hello"`
+}
+
+func main() {
+	apex.HandleFunc(func(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
+		var postItems []revelation.PostItem
+
+		postItems, err := revelation.Random()
+		if err != nil {
+			return nil, err
+		}
+
+		return postItems, nil
+	})
+}
